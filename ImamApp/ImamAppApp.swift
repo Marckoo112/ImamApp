ae83d74ab82e17af843009af9c1cffe7a76e00e2
imam <imamfathr@gmail.com>
2026-04-10 08:51:37 +0700

    Initial Commit

---
+++ ImamApp/ImamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImamAppApp.swift
+//  ImamApp
+//
+//  Created by Imam on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ImamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
